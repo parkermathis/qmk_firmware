82d31262bfd224b2e73fb7b5b98bf540f0d5679a
Parker <parker@mini-ion.local>
2025-05-06 12:26:26 -0400

    fixing kb left and right keys

--- keyboards/handwired/cosmotyl/keymaps/default/keymap.c
+++ keyboards/handwired/cosmotyl/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
             KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
             KC_NO, KC_END, KC_UP, KC_HOME, KC_F11,              KC_F12, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_NO, KC_RGHT, KC_DOWN, KC_LEFT, KC_NO,            KC_NO,  KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,
+            KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,            KC_NO,  KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,
             KC_NO, KC_PGDN, KC_NO, KC_PGUP, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_VOLD,
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_ESC, KC_DEL, COMBO, KC_NO, KC_NO,
             KC_NO, KC_NO,                                       KC_TAB, BKTAB
